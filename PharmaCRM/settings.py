c9db5454c8d68939f5e761fc852a19d96071f2b3
mwanyambu <jmwanyambu@gmail.com>
2024-01-22 13:44:21 +0300

    log in and out and registration

--- PharmaCRM/settings.py
+++ PharmaCRM/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'website'
+    'website', # Add this line to the list of installed apps
 ]
 
 MIDDLEWARE = [
